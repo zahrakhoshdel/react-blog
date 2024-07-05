22afb2dff288a0fa6ed4646298f5082bb0cf8ce1
unknown <imzahrakhoshdel@gmail.com>
2024-07-05 21:16:13 +0330

    adding data to the news section

---
+++ src/components/News/newsContentData.js
@@ -0,0 +1,93 @@
+import postImage1 from "../../assets/crossword.png";
+import postImage2 from "../../assets/Copy of Web Vs Mobile.png";
+import postImage3 from "../../assets/Building Better Software.png";
+import postImage4 from "../../assets/Deploying Fullstack Dart to AWS, Part 3-p-500.png";
+import postImage5 from "../../assets/Google IO-p-500.png";
+import postImage6 from "../../assets/Deploying Fullstack Dart to AWS, Part 2-p-500.png";
+import postImage7 from "../../assets/The Benefits of Streamlining Mobile & Web Development-p-500.png";
+import postImage8 from "../../assets/Copy of Building a custom app p-500.png";
+import postImage9 from "../../assets/Digital Escape Room 1-p-500.png";
+import postImage10 from "../../assets/Wrapped up in Apps-p-500.png";
+
+import avatar1 from "../../assets/Logomark Outlined.svg";
+import avatar2 from "../../assets/joshua-shewmaker.png";
+import avatar3 from "../../assets/Tom-Arra.png";
+import avatar4 from "../../assets/Stefan 2023-p-500.jpg";
+import avatar5 from "../../assets/Laura_Hughes-p-500.webp";
+import avatar6 from "../../assets/Jorge Coca-p-500.webp";
+import avatar7 from "../../assets/David Zemanek-p-500.webp";
+
+export const newsContentData = [
+  {
+    title: "How we built it: Creating the I/O Crossword puzzle, powered by AI",
+    image: postImage1,
+    date: "June 26, 2024",
+    author: "VGV Team",
+    avatar: avatar1,
+  },
+  {
+    title: "Building Better Software: The Importance of Rigorous Code Reviews",
+    image: postImage2,
+    date: "June 11, 2024",
+    author: "Joshua Shewmaker",
+    avatar: avatar2,
+  },
+  {
+    title: "Seamless Full Stack Development using Flutter & Dart Frog",
+    image: postImage3,
+    date: "June 3, 2024",
+    author: "Tom Arra",
+    avatar: avatar3,
+  },
+  {
+    title: "Deploying Fullstack Dart to AWS, Part III: Deploying to AWS",
+    image: postImage4,
+    date: "May 22, 2024",
+    author: "Stefan Hodges-Kluck",
+    avatar: avatar4,
+  },
+  {
+    title: "Google I/O 2024: Flutter Moving Forward",
+    image: postImage5,
+    date: "May 20, 2024",
+    author: "VGV Team",
+    avatar: avatar1,
+  },
+  {
+    title: "Deploying Fullstack Dart to AWS, Part II: Building the Web App",
+    image: postImage6,
+    date: "May 13, 2024",
+    author: "Stefan Hodges-Kluck",
+    avatar: avatar4,
+  },
+  {
+    title: "The Benefits of Streamlining Mobile & Web Development",
+    image: postImage7,
+    date: "May 9, 2024",
+    author: "Laura Hughes",
+    avatar: avatar5,
+  },
+  {
+    title: "Deploying Fullstack Dart to AWS: Part I",
+    image: postImage8,
+    date: "May 7, 2024",
+    author: "Stefan Hodges-Kluck",
+    avatar: avatar4,
+  },
+  {
+    title:
+      "How It’s Made: Digital Escape Room AI-driven game powered by Gemini and Flutter",
+    image: postImage9,
+    date: "April 29, 2024",
+    author: "Jorge Coca",
+    avatar: avatar6,
+  },
+  {
+    title:
+      "Wrapped Up in Apps: The Pros and Cons of Web Views in Mobile Development",
+    image: postImage10,
+    date: "April 23, 2024",
+    author: "David Zemanek",
+    avatar: avatar7,
+  },
+];
---
+++ src/components/News/newsTagData.js
@@ -0,0 +1,62 @@
+export const newsTagData = [
+  {
+    title: "All tags",
+    tag: "sll-tags",
+  },
+  {
+    title: "Business Value",
+    tag: "business-value",
+  },
+  {
+    title: "Full Stack",
+    tag: "full-stack",
+  },
+  {
+    title: "Dart",
+    tag: "dart",
+  },
+  {
+    title: "Webinar",
+    tag: "webinar",
+  },
+  {
+    title: "Best Practices",
+    tag: "best-practices",
+  },
+  {
+    title: "Enterprise",
+    tag: "enterprise",
+  },
+  {
+    title: "VGV",
+    tag: "vgv",
+  },
+  {
+    title: "Flutter",
+    tag: "flutter",
+  },
+  {
+    title: "Architecture",
+    tag: "architecture",
+  },
+  {
+    title: "Testing",
+    tag: "testing",
+  },
+  {
+    title: "Games",
+    tag: "games",
+  },
+  {
+    title: "Backend",
+    tag: "backend",
+  },
+  {
+    title: "Open Source",
+    tag: "open-source",
+  },
+  {
+    title: "Tutorial",
+    tag: "tutorial",
+  },
+];
