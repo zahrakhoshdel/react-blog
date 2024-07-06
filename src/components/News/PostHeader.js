964eab05c45ab69b8d6fab4c9e7da13093546dfe
unknown <imzahrakhoshdel@gmail.com>
2024-07-06 12:08:24 +0330

    refactoring

--- src/components/News/PostHeader.js
+++ src/components/News/PostHeader.js
@@ -2,13 +2,11 @@ import classes from "./PostHeader.module.css";
 
 const PostHeader = ({ image, title }) => {
   return (
-    <div className={classes.post}>
-      <a>
-        <img src={image} className={classes.image} />
-        <h3>{title}</h3>
-        <div className={classes["spacer-small"]}></div>
-      </a>
-    </div>
+    <a className={classes.post}>
+      <img src={image} className={classes.image} />
+      <h3>{title}</h3>
+      <div className={classes["spacer-small"]}></div>
+    </a>
   );
 };
 
